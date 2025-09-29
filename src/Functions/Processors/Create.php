88bddf73a438a1e44013a0ae0b013d5fd4430fcf
Patrick Morpey <patrick@morphp.nl>
2025-09-29 15:12:54 +0200

    Refactor `Create` processor to use service locator for `EntityCreator`

--- src/Functions/Processors/Create.php
+++ src/Functions/Processors/Create.php
@@ -10,18 +10,12 @@ use Medas\TestDataCreator\{Definitions\Definition, EntityCreator, Job};
 #[Service]
 readonly class Create
 {
-    public function __construct(
-        private EntityCreator $creator,
-    )
-    {
-    }
-
     public function process(Job $job, array $parameters): object
     {
         $defition = new Definition();
 
         $defition->entity = $parameters[0];
 
-        return $this->creator->createEntity($job, $defition, []);
+        return \service(EntityCreator::class)->createEntity($job, $defition, []);
     }
 }
