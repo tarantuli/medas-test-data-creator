c5a037675a6cad439330c26ca00f219ee245524e
Patrick Morpey <patrick@morphp.nl>
2026-06-15 12:24:35 +0200

    Update dependencies to latest versions and adjust `phpunit.bootstrap.php` to use `ServiceConfigBuilder` with `ObjectInstantiator`.

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
+use Medas\ObjectInstantiator\ObjectInstantiator;
+use Medas\ServiceManager\{ServiceConfigBuilder, ServiceManager};
 use Medas\TestDataCreator\TestDataCreatorPackage;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
 
 chdir(__DIR__);
 
-new ServiceManager(function (): ServiceConfig {
-    $config = new ServiceConfig();
+new ServiceManager(function (): ServiceConfigBuilder {
+    $config = new ServiceConfigBuilder(ObjectInstantiator::class);
 
     $config->addPackages([
         TestDataCreatorPackage::instance(),
